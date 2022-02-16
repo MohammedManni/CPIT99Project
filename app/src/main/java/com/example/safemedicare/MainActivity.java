e0c9d95cfe3fb49bf8b6aeaec0eeb0cc23eb96cd
Nawaf1990 <91433731+Nawaf1990@users.noreply.github.com>
2022-02-16 18:22:02 +0300

    set the interface

--- app/src/main/java/com/example/safemedicare/MainActivity.java
+++ app/src/main/java/com/example/safemedicare/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.privacy);
+        setContentView(R.layout.profile_caregiver);
     }
 }
