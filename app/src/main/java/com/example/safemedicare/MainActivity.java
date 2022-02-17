1365d7b127e6b89cda6233acd24d3d9a07897acf
Mohammed Manni <mmuna0002@stu.kau.edu.sa>
2022-02-17 14:37:44 +0300

    bar try

--- app/src/main/java/com/example/safemedicare/MainActivity.java
+++ app/src/main/java/com/example/safemedicare/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.safemedicare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.caregiver_profile);
+        setContentView(R.layout.home_page);
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        TextView profile= findViewById(R.id.firstB);
+
+
+toolbar.setActivated(true);
+
+
+
+
+
+
     }
 }
