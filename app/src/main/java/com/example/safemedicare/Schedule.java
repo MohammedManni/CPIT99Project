ba3598355751efccb6696773bd2ae2fc22481ac6
Mohammed Manni <mmuna0002@stu.kau.edu.sa>
2022-02-19 23:04:27 +0300

    creating Activities 1

---
+++ app/src/main/java/com/example/safemedicare/Schedule.java
@@ -0,0 +1,4 @@
+package com.example.safemedicare;
+
+public class Schedule {
+}
--- app/src/main/java/com/example/safemedicare/register2Activity.java
+++ app/src/main/java/com/example/safemedicare/register2Activity.java
@@ -20,7 +20,7 @@ public class register2Activity extends AppCompatActivity {
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(register2Activity.this, register1Activity.class);
+                Intent intent= new Intent(register2Activity.this, Home_Page.class);
                 startActivity(intent);
             }
         });
