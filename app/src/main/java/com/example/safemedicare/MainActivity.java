c87f5dc6743d3f699e5222af5b4cc17edc31532b
Mohammed Manni <mmuna0002@stu.kau.edu.sa>
2022-02-21 17:56:19 +0300

    up date some Layout and classes

--- app/src/main/java/com/example/safemedicare/MainActivity.java
+++ app/src/main/java/com/example/safemedicare/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.slpash);
+        setContentView(R.layout.medic_splash);
 
 
         // getSupportActionBar().hide();
