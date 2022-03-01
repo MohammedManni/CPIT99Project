8273a7e242933e9c6214103d39b72a3c76c0b257
Mohammed Manni <mmuna0002@stu.kau.edu.sa>
2022-03-01 13:55:23 +0300

    prepare for DB 0.1

--- app/src/main/java/com/example/safemedicare/db1BackgroundWorker.java
+++ app/src/main/java/com/example/safemedicare/db1BackgroundWorker.java
@@ -17,39 +17,41 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 
-public class db1BackgroundWorker extends AsyncTask<String,Void,String> {
+public class db1BackgroundWorker extends AsyncTask<String, Void, String> {
     Context context;
     AlertDialog alertDialog;
+
     db1BackgroundWorker(Context ctx) {
         context = ctx;
     }
+
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
         String login_url = "http://192.168.100.171/login.php";
         String register_url = "http://192.168.100.171/register.php";
-        if(type.equals("login")) {
+        if (type.equals("login")) {
             try {
                 String user_name = params[1];
                 String password = params[2];
                 URL url = new URL(login_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
-                                  +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
+                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                String result="";
-                String line="";
-                while((line = bufferedReader.readLine())!= null) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
                     result += line;
                 }
                 bufferedReader.close();
@@ -61,8 +63,7 @@ public class db1BackgroundWorker extends AsyncTask<String,Void,String> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else if(type.equals("register")) {
+        } else if (type.equals("register")) {
             try {
                 String name = params[1];
                 String surname = params[2];
@@ -72,26 +73,26 @@ public class db1BackgroundWorker extends AsyncTask<String,Void,String> {
 
                 URL url = new URL(register_url);
 
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"
-                        +URLEncoder.encode("surname","UTF-8")+"="+URLEncoder.encode(surname,"UTF-8")+"&"
-                        +URLEncoder.encode("age","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"
-                        +URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(age,"UTF-8")+"&"
-                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                String post_data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8") + "&"
+                        + URLEncoder.encode("surname", "UTF-8") + "=" + URLEncoder.encode(surname, "UTF-8") + "&"
+                        + URLEncoder.encode("age", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&"
+                        + URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(age, "UTF-8") + "&"
+                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                String result="";
-                String line="";
-                while((line = bufferedReader.readLine())!= null) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
                     result += line;
                 }
                 bufferedReader.close();
--- app/src/main/java/com/example/safemedicare/register1Activity.java
+++ app/src/main/java/com/example/safemedicare/register1Activity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,26 +16,25 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class register1Activity extends AppCompatActivity {
-    EditText Name, PhoneNumber,Age, Username,Password , ConfirmPassword;
+    EditText Name, PhoneNumber, Age, Username, Password, ConfirmPassword;
     RadioGroup radioGroup;
-    RadioButton  radioButtonP , radioButtonC;
-    Context context;
+    RadioButton radioButtonP, radioButtonC;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_1);
         // Edit text
-        Username = (EditText)findViewById(R.id.UserNameRE);
-        Name = (EditText)findViewById(R.id.PersonName);
-        PhoneNumber = (EditText)findViewById(R.id.PhoneNumber);
-        Age = (EditText)findViewById(R.id.Age);
-        Password = (EditText)findViewById(R.id.Password1);
-        ConfirmPassword = (EditText)findViewById(R.id.ConPassword1);
+        Username = (EditText) findViewById(R.id.UserNameRE);
+        Name = (EditText) findViewById(R.id.PersonName);
+        PhoneNumber = (EditText) findViewById(R.id.PhoneNumber);
+        Age = (EditText) findViewById(R.id.Age);
+        Password = (EditText) findViewById(R.id.Password1);
+        ConfirmPassword = (EditText) findViewById(R.id.ConPassword1);
         // Radio Button
-        radioGroup =(RadioGroup) findViewById(R.id.radioGroup);
-        radioButtonP= (RadioButton) findViewById(R.id.PatientRadioButton);
-        radioButtonC= (RadioButton) findViewById(R.id.CaregiverRadioButton2);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioButtonP = (RadioButton) findViewById(R.id.PatientRadioButton);
+        radioButtonC = (RadioButton) findViewById(R.id.CaregiverRadioButton2);
 
 
         // continue button
@@ -42,29 +42,48 @@ public class register1Activity extends AppCompatActivity {
         con.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //   String a1 = Username.getText().toString();
+                // check is all the input are filled
+                if ((Username.getText().toString().equalsIgnoreCase("") || Name.getText().toString().equalsIgnoreCase("") ||
+                        PhoneNumber.getText().toString().equalsIgnoreCase("") || Age.getText().toString().equalsIgnoreCase("") ||
+                        Password.getText().toString().equalsIgnoreCase("") || ConfirmPassword.getText().toString().equalsIgnoreCase(""))
+                        || (!radioButtonP.isChecked() && !radioButtonC.isChecked())) {
+                    // toast test to fill all the input
 
 
-                    if ( radioButtonP.isChecked()){
-                        // next register page Patient
-                        Intent intent = new Intent(register1Activity.this, register2Activity.class);
-                        startActivity(intent);
+                } else { // else whine all the input filled
+
+                    // the password match check
+                    if (Password.getText().toString().equals(ConfirmPassword.getText().toString())) {
+                        // the check if patient or care giver
+                        if (radioButtonP.isChecked()) {
+                            // next register page Patient
+                            Intent intent = new Intent(register1Activity.this, register2Activity.class);
+                            startActivity(intent);
 
 
-                    }else if ( radioButtonC.isChecked()){
-                        // Caregiver home page
-                        Intent intent = new Intent(register1Activity.this, caregiver_homePage_activity.class);
-                        startActivity(intent);
+                        } else if (radioButtonC.isChecked()) {
+                            // Caregiver home page
+                            Intent intent = new Intent(register1Activity.this, caregiver_homePage_activity.class);
+                            startActivity(intent);
 
-                    }else{
+                        } else {
+
+                        }
+
+                    } else {// else for not matching pass
 
                     }
+
+                }
 
             }
         });
 
+
         // Back button
         Button Back = findViewById(R.id.Back);
-        Back.setOnClickListener(new View.OnClickListener(){
+        Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(register1Activity.this, sign_activity.class);
