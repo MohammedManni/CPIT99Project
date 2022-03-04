c077089ea333681acdd852ec8a33ab64b6db13b8
Nawaf1990 <91433731+Nawaf1990@users.noreply.github.com>
2022-03-04 23:52:20 +0300

    update0.5(read medication from DB)

--- app/src/main/java/com/example/safemedicare/retriveDB.java
+++ app/src/main/java/com/example/safemedicare/retriveDB.java
@@ -23,30 +23,32 @@ import java.io.InputStreamReader;
 import java.net.URI;
 
 public class retriveDB extends AppCompatActivity {
-ListView list;
-ArrayAdapter<String> adapter;
+    ListView list;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrive_d_b);
 
 
-        list=(ListView)findViewById(R.id.list);
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-   list.setAdapter(adapter);
+        list = (ListView) findViewById(R.id.list);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
         new Connection().execute();
     }
-    class Connection extends AsyncTask<String,String,String>{
+
+    class Connection extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... strings) {
             String result = "";
             String medication_url = "http://192.168.100.10/readCaregiver.php";
             try {
-                HttpClient client=new DefaultHttpClient();
-                HttpGet request =new HttpGet();
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
                 request.setURI(new URI(medication_url));
-                HttpResponse response= client.execute(request);
+                HttpResponse response = client.execute(request);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
                 StringBuffer stringBuffer = new StringBuffer("");
                 String line = "";
@@ -55,39 +57,38 @@ ArrayAdapter<String> adapter;
                     break;
                 }
                 reader.close();
-                result=stringBuffer.toString();
+                result = stringBuffer.toString();
 
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 return new String("error");
             }
 
 
-
             return result;
         }
+
         @Override
         protected void onPostExecute(String result) {
             try {
-                JSONObject jsonResult=new JSONObject(result);
-                int success= jsonResult.getInt("success");
-                if (success ==1){
-                    JSONArray caregiverData= jsonResult.getJSONArray("caregiver");
-                    for (int i=0; i<caregiverData.length();i++){
-                        JSONObject caregiverObject=caregiverData.getJSONObject(i);
-                        int id= caregiverObject.getInt("id");
-                        String userName=caregiverObject.getString("userName");
-                        String name=caregiverObject.getString("name");
-                        int phoneNum=caregiverObject.getInt("phoneNumber");
-                        TextView t=(TextView) findViewById(R.id.textVi);
+                JSONObject jsonResult = new JSONObject(result);
+                int success = jsonResult.getInt("success");
+                if (success == 1) {
+                    JSONArray caregiverData = jsonResult.getJSONArray("caregiver");
+                    for (int i = 0; i < caregiverData.length(); i++) {
+                        JSONObject caregiverObject = caregiverData.getJSONObject(i);
+                        int id = caregiverObject.getInt("id");
+                        String userName = caregiverObject.getString("userName");
+                        String name = caregiverObject.getString("name");
+                        int phoneNum = caregiverObject.getInt("phoneNumber");
+                        TextView t = (TextView) findViewById(R.id.textVi);
                         t.setText(userName);
-                        String line=id+" - "+userName+" - "+name+" - "+phoneNum;
+                        String line = id + " - " + userName + " - " + name + " - " + phoneNum;
                         adapter.add(line);
 
                     }
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"no there",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "no there", Toast.LENGTH_SHORT).show();
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
