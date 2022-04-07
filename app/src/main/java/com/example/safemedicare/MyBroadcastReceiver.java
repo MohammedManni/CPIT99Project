a1f9ec2d034accfe20020d48a810ebab1c5720e4
Nawaf1990 <91433731+Nawaf1990@users.noreply.github.com>
2022-04-08 02:10:53 +0300

    notification

--- app/src/main/java/com/example/safemedicare/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/safemedicare/MyBroadcastReceiver.java
@@ -3,27 +3,20 @@ package com.example.safemedicare;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 public class MyBroadcastReceiver extends BroadcastReceiver  {
-    Context context;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
-        NotificationCompat.Builder builder = new NotificationCompat.Builder( context,"My Notification");
-        builder.setSmallIcon(R.drawable.ic_baseline_notifications_active_24);
-        builder.setContentTitle("Event Name: ");
-        builder.setContentText("Event Description: ");
-        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setAutoCancel(true);
+        Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
 
+        NotificationClass notificationClass = new NotificationClass(context);
+        NotificationCompat.Builder notification = notificationClass.getChannelNotification();
+        notificationClass.getManager().notify(1, notification.build());
 
-        // to notify the user
-        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
-        // help id give anything / build create notification
-        managerCompat.notify(1, builder.build());
 
 
     }
